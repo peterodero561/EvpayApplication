f657e4a0c459f506d3ac142fd7a9c3a8583d103f
Peter Odero <peterodero561@gmail.com>
2024-10-16 14:21:31 +0300

    update the init file for the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from .extensions import db, login_manager, migrate
-from .blueprints.auth import auth
-from .blueprints.profiles import profiles
-from .blueprints.payments import payments
+from .blueprints.auth.routes import auth_bp
+# from .blueprints.profiles.routes import profiles_bp
+# from .blueprints.payments.routes import payments_bp
+from app.config_app import Config
 
-def create_app(config_file=None):
+
+def create_app(config_name='config_app.py'):
     app = Flask(__name__)
-    
-    if config_file:
-        app.config.from_pyfile(config_file)
+    # configuring app with the database
+    app.config.from_object(Config)
 
     # Initialize extensions
     db.init_app(app)
@@ -16,8 +17,8 @@ def create_app(config_file=None):
     migrate.init_app(app, db)
 
     # Register blueprints
-    app.register_blueprint(auth)
-    app.register_blueprint(profiles)
-    app.register_blueprint(payments, url_prefix='/payments')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    # app.register_blueprint(profiles_bp, url_prefix='/auth')
+    # app.register_blueprint(payments_bp, url_prefix='/auth')
 
     return app
