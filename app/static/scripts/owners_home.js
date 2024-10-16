e97501f0cb7cc5d2468427e8c521d15b8543812a
Peter Odero <peterodero561@gmail.com>
2024-10-16 17:00:11 +0300

    added the app static directory

---
+++ app/static/scripts/owners_home.js
@@ -0,0 +1,15 @@
+function goBack() {
+    window.history.back('Home.html');
+}
+function evpay_account() {
+    window.location.href ='account_owners.html';
+}
+// Additional functionality can be added here for button clicks
+function battery_charging(){
+    window.location.href ='battery_payment.html';
+
+}
+
+function battery_maintenance(){
+    window.location.href ='battery_maintanance.html';
+}
