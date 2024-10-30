0f41784a4e299cca65f9c94f26b71624497271ce
Peter Odero <peterodero561@gmail.com>
2024-10-30 15:08:40 +0300

    update

--- app/static/scripts/profile.js
+++ app/static/scripts/profile.js
@@ -1,6 +1,6 @@
 // load profile data from server
 document.addEventListener('DOMContentLoaded', function() {
-    fetch_data();
+    fetch_data_user();
 });
 
 // function to go to home page
@@ -9,7 +9,7 @@ function goBack() {
 }
 
 //function to fetch data from server of current user
-function fetch_data() {
+function fetch_data_user() {
     fetch('/profiles/account_data', {method: 'GET'})
     .then(response => {
         if (!response.ok) {
@@ -19,24 +19,69 @@ function fetch_data() {
     })
     .then(data => {
         if (data.userRole === 'driver'){
-            document.getElementById('add-btn').style.display = 'inline-block';
+            fetch_data_bus(function(success){
+                if (success === 1) {
+                    document.getElementById('edit-bus-btn').style.display = 'inline-block';
+                } else {
+                    document.getElementById('add-btn').style.display = 'inline-block';
+                }
+            });
         }
         document.querySelector('.profile-pic').innerHTML = `
             <img id="profile-image" src="/static/images/profiles/${encodeURIComponent(data.userProfilePic || 'default.jpg')}" alt="Profile Picture">
         `;
 
         document.querySelector('.profile-info').innerHTML = `
-            <h3>My Information</h3>
+            <h2 style='color: green;'>My Information</h2>
             <p><strong>Name:</strong> <span id="name-display">${data.userName}</span></p>
             <p><strong>Email:</strong> <span id="email-display">${data.userEmail}</span></p>
             <p><strong>Designation:</strong> <span id="designation-display">EV ${data.userRole}</span></p>
         `;
     })
     .catch(error => {
-        console.error('Error fetching account infromation: ', error);
         document.querySelector('.profile-info').innerHTML = `
             <h2>Error ${error}</h2>
         `;
+    });
+}
+
+//function for fetch data of bus
+function fetch_data_bus(callback){
+    // fetch bus from server
+    fetch('/profiles/account_bus', {method: 'GET'})
+    .then(response => {
+        console.log('Response', response);
+        return response.json()
+    })
+    .then(data => {
+        if (data.busId == "Null") {
+            document.querySelector('.update-message').style.display = 'block';
+            document.querySelector('.update-message').innerHTML = `
+                <h3>You have not registered a bus</h3>
+            `;
+            // call the call back with zero to indicate failure
+            if (callback) callback(0);
+        } else {
+            document.querySelector('.bus-info').innerHTML = `
+                <h2 style='color: green;'>My Bus Information</h2>
+                <p><strong>Bus Plate:</strong> <span id="name-display"> ${data.busPlateNo}</span></p>
+                <p><strong>Bus Model:</strong> <span id="email-display"> ${data.busModel}</span></p>
+                <p><strong>Bus Battery Model:</strong> <span id="designation-display"> ${data.busBatteryModel}</span></p>
+                <p><strong>Bus Battery Company:</strong> <span id="designation-display"> ${data.busBatteryCompany}</span></p>
+                <p><strong>Bus Battery Seats:</strong> <span id="designation-display"> ${data.busSeatsNo}</span></p>
+            `;
+            // call the call back with one to indicate success
+            if (callback) callback(1);
+        }
+    })
+    .catch(error =>{
+        console.error('Error: ', error);
+        document.querySelector('.update-message').style.display = 'block';
+        document.querySelector('.update-message').innerHTML = `
+            <h3>Error Fetching Bus Information</h3>
+        `;
+        // call the call back with zero to indicate failure
+        if (callback) callback(0);
     });
 }
 
@@ -51,6 +96,7 @@ document.getElementById('edit-btn').addEventListener('click', function () {
     document.getElementById('edit-btn').style.display = 'none';
     document.getElementById('save-btn').style.display = 'inline-block';
 });
+
 
 // Save the changes and update display
 document.getElementById('save-btn').addEventListener('click', function () {
@@ -72,7 +118,7 @@ document.getElementById('save-btn').addEventListener('click', function () {
         document.getElementById('save-btn').disabled = true;
 
         fetch('/profiles/account_update', {
-            method: 'POST',
+            method: 'PUT',
             body: formData
         })
         .then(response => response.json())
@@ -103,6 +149,81 @@ document.getElementById('add-btn').addEventListener('click', function() {
     document.querySelector('.profile-info').style.display = 'none';
     document.querySelector('.profile-pic').style.display = 'none';
     document.getElementById('edit-btn').style.display = 'none';
+    document.getElementById('add-btn').style.display = 'none';
 
     document.querySelector('#add-bus-form').style.display = 'block';
+    document.getElementById('save-bus-btn').style.display = 'inline-block';
+});
+
+// listner for save bus button
+document.getElementById('save-bus-btn').addEventListener('click', function() {
+    document.getElementById('save-bus-btn').disabled = true;
+    const form = document.getElementById('add-bus-form');
+    const formData = new FormData(form);
+
+    fetch('/auth/register_bus', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.code == 200){
+            window.location.href = '/profiles/account';
+        } else if (data.code == 403){
+            document.querySelector('.update-message').style.display = 'block';
+            document.getElementById('save-bus-btn').disabled = false;
+            document.querySelector('.update-message').innerHTML = `
+                <h2>Error ${data.message}</h2>
+            `;
+        }
+    })
+    .catch(error => {
+        document.querySelector('.update-message').style.display = 'block';
+        document.getElementById('save-bus-btn').disabled = false;
+        document.querySelector('.update-message').innerHTML = `
+            <h2>Error ${error}</h2>
+        `;
+    });
+});
+
+//event listener for the edit bus button
+document.getElementById('edit-bus-btn').addEventListener('click', function() {
+    document.querySelector('.profile-info').style.display = 'none';
+    document.querySelector('.profile-pic').style.display = 'none';
+    document.getElementById('edit-btn').style.display = 'none';
+    document.getElementById('edit-bus-btn').style.display = 'none';
+
+    document.getElementById('add-bus-form').style.display = 'block';
+    document.getElementById('save-edit-bus-btn').style.display = 'inline-block';
+});
+
+// event listners to add listner to the save-edit bus button
+document.getElementById('save-edit-bus-btn').addEventListener('click', function(){
+    document.getElementById('save-edit-bus-btn').disabled = true;
+    const form = document.getElementById('add-bus-form');
+    const formData = new FormData(form);
+
+    fetch('/profiles/account_bus_update', {
+        method: 'PUT',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.code == 200){
+            window.location.href = '/profiles/account';
+        } else if (data.code == 403){
+            document.querySelector('.update-message').style.display = 'block';
+            document.getElementById('save-bus-btn').disabled = false;
+            document.querySelector('.update-message').innerHTML = `
+                <h2>Error ${data.message}</h2>
+            `;
+        }
+    })
+    .catch(error => {
+        document.querySelector('.update-message').style.display = 'block';
+        document.getElementById('save-bus-btn').disabled = false;
+        document.querySelector('.update-message').innerHTML = `
+            <h2>Error ${error}</h2>
+        `;
+    });
 });
