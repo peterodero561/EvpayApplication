26538828b0e62aea9c62bcb641e5b76f3a6c04b0
Peter Odero <peterodero561@gmail.com>
2025-03-01 14:17:56 +0300

    modified Footer component to fit all screens and not only User scree

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { useRoute } from "@react-navigation/native";
 
-const Footer = ({navigation, user, menu}) => {
+const Footer = ({navigation, user, menu, menuVisible}) => {
+    // get the current route name from react navigation
+    const route = useRoute()
+
+    // Function to return the appropriate color base on active route
+    const getIconColor = (screenName) => {
+        return route.name === screenName ? '#007AFF' : '#8e8e8e';
+    };
+
     return(
         <View style={styles.footer}>
-            <TouchableOpacity onPress={() => navigation.navigate('UserScreen')} style={styles.footerItem} >
-                <Icon name="home" size={30} color='#007AFF' />
+            <TouchableOpacity onPress={() => navigation.navigate('UserScreen', { user: user })} style={styles.footerItem} >
+                <Icon name="home" size={30} color={getIconColor('UserScreen')} />
                 <Text style={styles.footerText}>Home</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('Profile', { user: user })} style={styles.footerItem} >
-                <Icon name="person" size={30} color='#8e8e8e' />
+                <Icon name="person" size={30} color={getIconColor('Profile')} />
                 <Text style={styles.footerText}>Profile</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={menu} style={styles.footerItem} >
-                <Icon name="menu" size={30} color='#8e8e8e' />
+                <Icon name="menu" size={30} color={menuVisible ? "#007AFF": "#8e8e8e"} />
                 <Text style={styles.footerText}>Menu</Text>
             </TouchableOpacity>
         </View>
